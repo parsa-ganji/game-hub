7587a8ae5b046d52d3b444ad2f7a9d27319789c7
Parsa <parsa0.ganji0@gmail.com>
2025-05-31 16:18:55 +0330

    Sort the games.

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,19 +1,39 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectedOrder: (sortOrder: string) => void;
+  selectedOrder: string;
+}
+
+const SortSelector = ({ onSelectedOrder, selectedOrder }: Props) => {
+  const sortOrder = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average rating" },
+  ];
+
+  const currentSortOrder = sortOrder.find(
+    (order) => order.value === selectedOrder
+  );
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Order by: Relevance
+        Order by: {currentSortOrder?.label || "Relevance"}
       </MenuButton>
       <MenuList>
-        <MenuItem>Relevense</MenuItem>
-        <MenuItem>Date added</MenuItem>
-        <MenuItem>Name</MenuItem>
-        <MenuItem>Release date</MenuItem>
-        <MenuItem>Popularity</MenuItem>
-        <MenuItem>Average rating</MenuItem>
+        {sortOrder.map((order) => (
+          <MenuItem
+            key={order.value}
+            value={order.value}
+            onClick={() => onSelectedOrder(order.value)}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -17,6 +17,6 @@ export interface Game {
 }
 
 
-const useGames = (gameQuery: GameQuery) => useData<Game>('/games',{params: {genres: gameQuery.genre?.id,platforms: gameQuery.platform?.id}},[gameQuery.genre?.id, gameQuery.platform?.id]);
+const useGames = (gameQuery: GameQuery) => useData<Game>('/games',{params: {genres: gameQuery.genre?.id,platforms: gameQuery.platform?.id,ordering: gameQuery.sortOrder}},[gameQuery.genre?.id, gameQuery.platform?.id, gameQuery.sortOrder]);
 
 export default useGames;
