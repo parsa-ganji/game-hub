23154daa59ab080ed3edb040e07db81b543ae123
Parsa <parsa0.ganji0@gmail.com>
2025-06-14 15:57:05 +0330

    Refactor fetch geners whit React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,7 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import useData, { FetchResponse } from "./useData";
+import apiClient from "../services/api-client";
+import genres from "../data/genres";
 
 
 export interface Genre {
@@ -8,7 +11,15 @@ export interface Genre {
 }
 
 
-const useGenres = () => useData<Genre>('/genres');
+const useGenres = () => useQuery({
+  queryKey: ['genre'],
+  queryFn: () => apiClient
+    .get<FetchResponse<Genre>>('/genres')
+    .then(res => res.data),
+  staleTime: 24 * 60 * 60 * 1000, // 24h
+  initialData:{count: genres.length, results: genres}
+
+});
 
 
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import App from "./App";
 import "./index.css";
 import theme from "./theme";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
