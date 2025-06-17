8144cb31e2e95a654fb1d0ec80d96c815fe620cc
Parsa <parsa0.ganji0@gmail.com>
2025-06-17 21:16:48 +0330

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -29,7 +29,8 @@ const useGames = (gameQuery: GameQuery) =>
   }}),
   getNextPageParam: (lastPage,allPage) => {
     return lastPage.next ? allPage.length + 1 : undefined;
-  }
+  },
+  staleTime: 24 * 60 * 60 * 1000
   });
 
 
