7f49766d6ff14510a7e71da4667081f40f283401
Parsa <parsa0.ganji0@gmail.com>
2025-08-24 22:12:36 +0330

    Refactor entity interface

--- src/components/GameAttribute.tsx
+++ src/components/GameAttribute.tsx
@@ -2,7 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import React from "react";
 import CriticScore from "./CriticScore";
 import DfinitionItem from "./DfinitionItem";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 
 interface Props {
   game: Game;
--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -2,7 +2,7 @@ import { Heading } from "@chakra-ui/react";
 import React from "react";
 
 import usePlatforms from "../hooks/usePlatforms";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 import useGenres from "../hooks/useGenres";
 import usePlatform from "../hooks/usePlatform";
 import useGenre from "../hooks/useGenre";
